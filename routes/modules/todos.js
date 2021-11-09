c382df388d0ebae15df4fb767acb27352e271d70
Sin0001 <bboyliljie@gmail.com>
2021-11-09 17:15:46 +0800

    feat: add update todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-// 新增 todo
+// 新增 todo 頁面
 router.get('/new', (req, res) => {
   return res.render('new')
 })
@@ -18,7 +18,7 @@ router.post('/', (req,res) => {
     .catch(error => console.log(error))
 })
 
-// 瀏覽特定 todo
+// 瀏覽特定 todo 頁面
 router.get('/:id', (req, res) => {
   const userId = req.user.id
   const id = req.params.id
@@ -26,6 +26,30 @@ router.get('/:id', (req, res) => {
     where: { id, userId}
   })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// 修改 todo 頁面
+router.get('/:id/edit', (req,res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: {id, UserId}})
+    .then( todo =>  res.render('edit', { todo: todo.get() }))
+    .catch(error => console.log(error))
+})
+
+// 送出修改 todo
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
