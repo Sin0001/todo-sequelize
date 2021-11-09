3781c495cb2444bb04e7ddc9aff88915dad7cddd
Sin0001 <bboyliljie@gmail.com>
2021-11-09 17:03:01 +0800

    feat: add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,7 +4,21 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-// 瀏覽特定todo
+// 新增 todo
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+// 送出新增 todo
+router.post('/', (req,res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// 瀏覽特定 todo
 router.get('/:id', (req, res) => {
   const userId = req.user.id
   const id = req.params.id
